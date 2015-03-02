d7ea2abdce8de800fe28a2501f4a9ce157dcd8df
avi <avinashsajjan@gmail.com>
2015-03-02 17:39:58 +0530

    added code which segues and passes the image url which needs to be loaded in detail view.

--- Cassini/ViewController.swift
+++ Cassini/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let ivc = segue.destinationViewController as? ImageViewController {
+            if let identtifier = segue.identifier {
+                switch identtifier {
+                    case "Earth":
+                        ivc.imageURL = DemoURL.NASA.Earth
+                        ivc.title = "Earth"
+                    case "Cassini":
+                        ivc.imageURL = DemoURL.NASA.Cassini
+                        ivc.title = "Cassini"
+                    case "Saturn":
+                        ivc.imageURL = DemoURL.NASA.Saturn
+                        ivc.title = "Saturn"
+                    default: break
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
